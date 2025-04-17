c22af9246c0cb4621d99cf5a7f439530e1ef39ed
Iñaki Gauthier <inakigauthier92@gmail.com>
2025-04-17 17:10:34 +0200

    feat(insights): add customer investment insights endpoint with controller and tests

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'customers/:id/portfolios', to: 'portfolios#index'
+      get 'customers/:id/insights', to: 'insights#show'
   
       post 'customers/:customer_id/portfolios/:id/deposit', to: 'portfolios#deposit'
       post 'customers/:customer_id/portfolios/:id/withdraw', to: 'portfolios#withdraw'
